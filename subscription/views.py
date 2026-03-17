da09e8f0b9d26c589a9f1f494adc70b414413220
kaifarikman <kna.zevs@yandex.ru>
2026-03-18 01:40:04 +0300

    chore(subscription.views): add subscription views

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Subscription
+from .serializers import SubscriptionSerializer
+
 
 # Create your views here.
+
+
+class SubscriptionPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 50
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    pagination_class = SubscriptionPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["title"]
+    ordering_fields = ["id", "monthly_price", "title"]
