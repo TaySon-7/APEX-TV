a38b31920dec9e77c4f253b1d6cd7ed9d8940f2b
kaifarikman <kna.zevs@yandex.ru>
2026-03-17 23:45:53 +0300

    chore(subscription.admin): add admin model

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subscription
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "monthly_price", "max_video_quality",)
+    search_fields = ("title",)
+    list_filter = ("max_video_quality",)
+
