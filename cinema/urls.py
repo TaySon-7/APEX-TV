796ba2fa4686f9ef2a4c1347f65ed00b02d88f2a
kaifarikman <kna.zevs@yandex.ru>
2026-03-18 01:42:04 +0300

    chore(urls): add subscription urls for cinema project

--- cinema/urls.py
+++ cinema/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("movie.urls"))
+    path("api/", include("movie.urls")),
+    path("api/", include("subscription.urls")),
 ]
