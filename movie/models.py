0112cebffe687f1119fe90db891c1f5b165d79b1
Кобылкевич Фёдор <fmkobylkevich@mai.education>
2026-03-22 20:30:25 +0300

    add ManyToManyField field subscriptions to model Movie. Structurize settings

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from subscription.models import Subscription
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
@@ -7,9 +9,15 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genre = models.CharField(max_length=200)
     release_date = models.DateField()
+    subscriptions = models.ManyToManyField(Subscription)
     poster_url = models.URLField()
     trailer_url = models.URLField()
     film_url = models.URLField()
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
 
     def __str__(self):
         return self.title
