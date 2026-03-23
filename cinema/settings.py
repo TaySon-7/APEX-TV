be8d1e4f4f1d682039c5d4e6272dcd58289a23f7
andreysk0304 <andreysk0304@yandex.ru>
2026-03-23 17:02:02 +0300

    edit settings and urls

--- cinema/settings.py
+++ cinema/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "movie",
     "subscription",
+    "users",
+    "watchlist",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
